9171c368ef52b024ba379bbfe957ab78a73d2e67
jcalcivarg <jcalcivar@hotmail.com>
2024-08-05 19:51:20 -0500

    ejericio.py y test

--- ejercicio.py
+++ ejercicio.py
@@ -5,7 +5,6 @@ compras = input("Ingrese una lista de compras: ")
 productos = compras.split(", ")
 print(f"Los productos en la lista de compras son: {productos}")
 
-
 # Convertir la lista de compras en una tupla
 def convertir_lista_a_tupla(lista):
     return   tuple(lista)  # Utiliza una función para convertir 
